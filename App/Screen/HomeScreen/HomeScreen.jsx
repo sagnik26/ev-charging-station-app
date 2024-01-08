4a8290adde8a5f441b59aa21979118414b59acf5
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2024-01-08 11:48:31 +0530

    Added homeScreen name and filter logo

--- App/Screen/HomeScreen/HomeScreen.jsx
+++ App/Screen/HomeScreen/HomeScreen.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import AppMapView from './AppMapView'
+import Header from './Header'
 
 const HomeScreen = () => {
   return (
-    <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <View >
+        <View style={styles.headerContainer}>
+          <Header/>
+        </View>
         <AppMapView />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: 'absolute',
+    zIndex: 10,
+    padding: 10,
+    width: '100%',
+    paddingHorizontal: 10,
+  }
+})
 
 export default HomeScreen
--- App/Utils/Colors.js
+++ App/Utils/Colors.js
@@ -2,6 +2,7 @@ export default {
     PRIMARY: "#0BC224",
     GRAY: "#898989",
     WHITE: "#ffffff",
-    BLACK: '#000000'
+    BLACK: '#000000',
+    WHITE_TRANSP: "#ffffff87"
 }
 
